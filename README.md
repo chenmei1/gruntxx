88f844a5e892f7b73f3677229a439a4ab5581dcd
chenmei1 <912749558@qq.com>
2015-05-14 21:59:28 +0800

    update grtuntfile.js and README.md

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ gruntxx
 
 Grunt 示例项目。
 
-文章：<http://yujiangshui.com/grunt-basic-tutorial/>
+来自文章：<http://yujiangshui.com/grunt-basic-tutorial/>
+
+知道了怎么去在node的npm的环境下面去部署grunt，来由通过grunt去安装各种的插件，在安装了插件了过后，再去配置gruntfile的文件，去安装配置grunt的初始环境，再去调用相应的插件，再去执行相应插件的功能，知道了问什么自动化能够解决很多的合并concat、压缩uglify、sass编译成css、js测试jshint、watch等等，
+
+
+
--- gruntfile.js
+++ gruntfile.js
@@ -30,6 +30,7 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    //grunt -- sass  change sass to css
     sass: {
       output : {
         options: {
@@ -39,12 +40,80 @@ module.exports = function(grunt) {
           './style.css': './scss/style.scss'
         }
       }
+    },
+    //grunt  ---concat many file to one file  
+    concat:{
+      options:{
+        separator:':',
+      },
+      dist:{
+        src:['./src/plungin.js','./src/plungin2.js'],
+        dest:'./global.js',
+      },
+    },
+    //grunt -- uglify make file  smallly
+    uglify:{
+      compress:{
+        files:{
+          './global.min.js':['./global.js']
+        }
+      }
+    },
+    //grunt --jshint  check js 
+    jshint:{
+      all:['./global.js']
+    },
+    //grunt --watch js and css change 
+    watch:{
+      script:{
+        files:['./src/plungin.js','./src/plungin2.js'],
+        tasks:['concat','jshint','uglify']
+      },
+      sass:{
+        files:['./scss/style.scss'],
+        tasks:['sass']
+      },
+      livereload:{
+        option:{
+          livereload:'<% = connect.options.livereload %>'
+        },
+        files:[
+          'index.html',
+          'style.css',
+          'js/global.min.js'
+        ]
+      }
+    },
+    connect:{
+      options:{
+        port:9000,
+        open:true,
+        livereload:356729,
+        //change this to '0.0.0.0' to access the server from outside
+        hostname:'localhost'
+      },
+      sever:{
+        options:{
+          port:9001,
+          base:'./'
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   grunt.registerTask('outputcss',['sass']);
+  grunt.registerTask('concatjs',['concat']);
+  //注册一个compress任务的目的就是依次执行合并、检查、压缩的任务。
+  grunt.registerTask('compress',['concat','jshint','uglify']);
+  grunt.registerTask('watchit',['sass','concat','jshint','connect','watch']);
+
   grunt.registerTask('default');
 
 };
